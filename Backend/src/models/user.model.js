656d40280b95904fc414a91c170a731353c90b2a
me-sayanghosh <sayanghosh1887@gmail.com>
2026-05-12 18:57:25 +0530

    user Schema done

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,   
+        required: true,
+    },
+    avater: {
+        type: String,
+        required : false,
+        default :function() {
+            return getGravatarUrl(this.email);
+        }
+    },
+
+});
+
+
+function getGravatarUrl(email) {
+    const hash = require('crypto').createHash('md5').update(email.trim().toLowerCase()).digest('hex');
+    return `https://www.gravatar.com/avatar/${hash}?d=identicon`;
+}
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
+
