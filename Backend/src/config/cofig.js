204d4686a855a9cdd11a6fa51849cc5e5e31df22
me-sayanghosh <sayanghosh1887@gmail.com>
2026-05-20 21:22:36 +0530

    fix: update sameSite attribute in cookieOptions based on environment

--- Backend/src/config/cofig.js
+++ Backend/src/config/cofig.js
@@ -1,6 +1,6 @@
 export const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 1000 * 60 * 60 * 24 * 30
 };
